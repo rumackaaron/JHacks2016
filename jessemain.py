7701dba3aa1779f3a73bb149c6e4bc6207d0276b
jesseschloss <jesse.schloss@eyestreet.com>
2016-02-14 11:20:14 -0500

    made my testing script

---
+++ jessemain.py
@@ -0,0 +1,24 @@
+from StudentMaker import getStudents
+import Scheduler
+import time
+def main():
+    
+    start = time.time()
+    students, course_obj_dict = getStudents(1000)
+    print 'Created students'
+    
+    all_courses = course_obj_dict.values()
+
+    initialThingy = Scheduler.initializeSchedule(all_courses)
+    print 'initialized schedule'
+    
+    tentSchedule = Scheduler.scheduler(1000, initialThingy)
+    print 'minimized conflicts in schedule'
+    
+    postSchedule = Scheduler.postProcess(tentSchedule)
+    end = time.time()
+    print 'output schedule for preference maximization'
+
+    print end-start
+
+main()
