d52e38964fd08ebe1d5c68a8d3ed5bffc899b887
rumackaaron <aaron@getneema.com>
2016-02-14 08:43:06 -0500

    Get section info

--- ClassData.py
+++ ClassData.py
@@ -3,13 +3,9 @@ FINAL_SLOTS = 20
 class Course:
 
     def __init__(self, data):
-        self.department = data['department']
         self.course = data['class']
-        self.semester = data['semester']
-        self.title = data['title']
         self.students = []
         self.sections = []
-        return self
 
 
 class Section:
@@ -18,15 +14,12 @@ class Section:
         self.end = data['endtime']
         self.start = data['starttime']
         self.section = data['section']
-        self.room = data['room']
         self.days = data['days']
-        self.building = data['building']
-        self.date = data['date']
-        self.semester = data['semester']
-        self.open = data['open']
         self.name = data['class']
-        self.professor = data['professor']
-        self.seats = data['seats']
+
+    def to_dict(self):
+        return {'endtime': self.end, 'starttime': self.start, 'section': self.section
+                , 'days': self.days, 'class': self.name}
 
 
 class Student:
